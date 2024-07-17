fca2c35c69ff93e1b646120ebe233efc7a4f772c
SirGay0v0 <serega112015@gmail.com>
2024-07-17 19:48:53 +0300

    Checkstyle 1.0

--- src/test/java/ru/practicum/shareit/RESTEndpointsTest/ItemControllerTest.java
+++ src/test/java/ru/practicum/shareit/RESTEndpointsTest/ItemControllerTest.java
@@ -9,7 +9,6 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
